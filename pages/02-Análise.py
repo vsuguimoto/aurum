80a8427d9d2349555b62388bbc5c0a63ebf0934b
Vinicius Suguimoto <vinisuguimoto@gmail.com>
2022-10-22 21:57:28 -0400

    create setup page

--- pages/02-Análise.py
+++ pages/02-Análise.py
@@ -1,8 +1,8 @@
 import streamlit as st
-import yfinance as yf
-import plotly.express as px
-import pandas as pd
-import numpy as np
+from src.data.data import get_ohlcv
+from src.features.ft import technical_indicators, traduzir_nome_colunas
+from src.models.model_training import create_setup
+from src.utils.utils import display_setup, svg_write
 
 TITULO_PROJETO = 'Aurum Praesagio'
 
@@ -15,62 +15,55 @@ st.set_page_config(
 
 def main_analysis():
 
-    st.markdown('## Sobre')
+    st.markdown('## Crie seu própio setup')
     st.markdown('''
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras porttitor mollis tincidunt.
     Sed fermentum elit lorem, eu finibus nisl maximus nec. Sed eget massa eget ligula blandit tincidunt eu eget lacus.
     ''')
     st.markdown('---')
 
-    st.markdown('**Selecione uma ação para ser analisada**')
+    st.markdown('**Digite o Ticker da ação**')
     
     ANALISE = st.text_input(' ')
+
+    st.markdown('**Selecione os indicadores**')
+    INDICADORES = st.multiselect(
+        label=' ',
+        options=[
+        'Distância da Média Móvel Exponencial de 9 períodos',
+        'Distância da Média Móvel Exponencial de 21 períodos',
+        'Preço fechou fora da Banda de Bollinger Superior',
+        'Preço fechou fora da Banda de Bollinger Inferior',
+        'Indice de Força Relativa de 14 períodos',
+        'Rate of Change de 5 dias',
+        'Oscilador Estocástico rápido 14-3-3',
+        ],
+
+    )
     
-    if ANALISE != '':
-        st.plotly_chart(grafico_preco(ANALISE))
-        st.plotly_chart(histograma_preco(ANALISE))
-        
+    if st.button('Criar Setup'):
+
+        st.markdown(f'## Setup para {ANALISE}')
+        DATA = get_ohlcv(ANALISE)
+        DATA = technical_indicators(DATA)
+        DATA = traduzir_nome_colunas(DATA)
+
+        TRAIN_SIZE = int(len(DATA) * .75)
+
+        X_train = DATA.loc[TRAIN_SIZE:, INDICADORES]
+        y_train = DATA.loc[TRAIN_SIZE:, 'Alvo']
+
+        MODEL = create_setup(X_train, y_train)
+
+        TREE_VIZ = display_setup(MODEL, X_train, y_train)
+
+        SVG = TREE_VIZ.svg()
+
+        svg_write(SVG)
+
+
     pass
 
-
-
-def dados_acoes(ticker):
-    
-    ticker = yf.Ticker(ticker)
-    
-    data_d = ticker.history(
-    period='5y',
-    interval='1d'
-    ).reset_index()
-    
-    # Cálculo do Retorno
-    data_d['Return'] = ((data_d['Close'] - data_d['Open'] + data_d['Dividends'])/data_d['Open']) * 100
-    
-    return data_d
-
-def grafico_preco(ticker):
-    
-    dados = dados_acoes(ticker)
-    
-    fig = px.line(
-        data_frame=dados,
-        x='Date',
-        y='Close',
-        title=f'Preço de Fechamento da {ticker}'
-    )
-    
-    return fig
-
-def histograma_preco(ticker):
-    
-    dados = dados_acoes(ticker)
-    
-    fig = px.histogram(
-        data_frame=dados,
-        x='Return',
-        title=f'Retorno da {ticker}',
-    )
-    return fig
 
 if __name__=='__main__':
     main_analysis()
